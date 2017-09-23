733899fffca9e447e4ed36775d77a79c10669369
Michael Hopkins <mahgdd@gmail.com>
2017-09-23 18:05:22 -0500

    Remove it entirely

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: './handler.js',
   target: 'node',
-  externals: {nodeExternals()},
   module: {
     loaders: [{
       test: /\.js$/,

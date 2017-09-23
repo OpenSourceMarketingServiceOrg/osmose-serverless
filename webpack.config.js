38b4cddcc12fdf819e4208122505fdf484945519
Michael Hopkins <mahgdd@gmail.com>
2017-09-23 18:42:30 -0500

    3rd time.

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,17 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 var nodeExternals = require('webpack-node-externals');
 
+node: {
+    __dirname: true
+}
+
 module.exports = {
 entry: './handler.js',
    target: 'node',
    externals: [nodeExternals()],
    output: {
       libraryTarget: 'commonjs',
-      path: __dirname + '/build'
+      path: path.join(__dirname, 'dist')
       filename: 'handler.js', // this should match the first part of function handler in serverless.yml
    },
   module: {

8892cebc90833573f8966c273af1ea2b38bd8baa
Michael Hopkins <mahgdd@gmail.com>
2017-09-23 15:10:10 -0500

    Do some configuring.

--- handler.js
+++ handler.js
@@ -1,16 +1,5 @@
 'use strict';
 
 module.exports.saveEmailStatus = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+ console.log("event", event);
 };

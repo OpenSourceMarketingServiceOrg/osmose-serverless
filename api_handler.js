173771d62f21c0e4f37d85629849b1f176eed7cd
Michael Hopkins <mahgdd@gmail.com>
2017-09-23 22:31:54 -0500

    Implement post, put, delete, get.

--- api_handler.js
+++ api_handler.js
@@ -1,20 +1,31 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 module.exports.emailList = (event, context, callback) => {
 
   console.log("event.httpMethod", event.httpMethod);
-
   var response;
-
+  var items;
   if(event.httpMethod === "GET") {
+    getItemsFromDynamoDB().then((res) => {
+      console.log("resForGet", res);
+      items = res.Items;
+    })
+    
     response = {
       statusCode: 200,
       body: JSON.stringify({
         message: "GET IT!",
-        input: event,
+        emails: items
       }),
     };
   } else if (event.httpMethod === "POST") {
+      postToDynamoDB(translateToPostParams(event)).then((res) => {
+        console.log("resForPost", res);
+      });
+
     response = {
       statusCode: 200,
       body: JSON.stringify({
@@ -23,6 +34,9 @@ module.exports.emailList = (event, context, callback) => {
       }),
     };
   } else if (event.httpMethod === "PUT") {
+    postToDynamoDB(translateToPostParams(event)).then((res) => {
+        console.log("resForPut", res);
+      });
     response = {
       statusCode: 200,
       body: JSON.stringify({
@@ -31,6 +45,10 @@ module.exports.emailList = (event, context, callback) => {
       }),
     };
   } else if (event.httpMethod === "DELETE") {
+    deleteItemFromDynamoDB(translateToDeleteParams(event)).then((res) => {
+      console.log("resForDelete", res);
+       });
+    
     response = {
       statusCode: 200,
       body: JSON.stringify({
@@ -50,3 +68,96 @@ module.exports.emailList = (event, context, callback) => {
 
   callback(null, response);
 };
+
+function postToDynamoDB(params) {
+  return new Promise((resolve, reject) => {
+  
+    dynamodb.updateItem(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        console.log("These params were rejected: ", params);
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function deleteItemFromDynamoDB(params) {
+    return new Promise((resolve, reject) => {
+  
+    dynamodb.deleteItem(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        console.log("These params were rejected: ", params);
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function getItemsFromDynamoDB() {
+ var params = {
+        TableName:"EmailList",
+        ProjectionExpression: "Email"
+     }; 
+
+   return new Promise((resolve, reject) => {
+  
+    dynamodb.scan(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        console.log("These params were rejected: ", params);
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function translateToPostParams(event) {
+  var body = JSON.parse(event.body);
+  var emailBinary = new Buffer(body.email).toString("base64")
+  return {
+    "Key": {
+      "EmailBinary": {
+        B: emailBinary
+      }
+    },
+    "ReturnValues":"NONE",
+    "ExpressionAttributeNames": {
+      "#FN": "FirstName",
+      "#LN": "LastName",
+      "#EM": "Email"
+    },
+    "ExpressionAttributeValues": {
+      ":fn": {
+        "S":body.firstName
+      },
+      ":ln": {
+        "S":body.lastName
+      },
+      ":em": {
+        "S":body.email
+      }
+    },
+    "UpdateExpression": "SET #FN = :fn, #LN = :ln, #EM = :em",
+      "TableName": "EmailList"
+    };
+}
+
+function translateToDeleteParams(event) {
+  var emailBinary = new Buffer(event.queryStringParameters.email).toString("base64")
+  return {
+    "Key": {
+      "EmailBinary": {
+        B: emailBinary
+      }
+    },
+    "TableName": "EmailList"
+    };
+}

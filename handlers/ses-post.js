f2c189fe983bfb623cbff6caab59ead320bf0a68
qtlawrence <qtlawrence@outlook.com>
2017-10-15 10:02:29 -0500

    SES POST

--- handlers/ses-post.js
+++ handlers/ses-post.js
@@ -3,8 +3,24 @@ const corsHeaders = {
     'Access-Control-Allow-Credentials' : true // Required for cookies, authorization headers with HTTPS
   }
 
+const osmose = require('osmose-email-engine');
+
 module.exports.postEmail = (event, context, callback) => {
     console.log("Going to post a great email!");
+    console.log(event.body);
 
+    let addresses = {
+        ToAddresses: event.body.to
+      };
+    let email = {
+        subject: event.body.subject,
+        body: event.body.content
+    };
+    let from = 'opensourcemarketingservice@gmail.com';
 
+    console.log('osmose: ', osmose);
+    console.log('addresses: ', addresses);
+    console.log('email: ', email);
+    
+    osmose.osmoseSendEmail(addresses, email, from);
 }
